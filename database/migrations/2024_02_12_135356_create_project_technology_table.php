de6de8b909e15915553c787d122cfc1ede41e515
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-12 17:27:43 +0100

    feat: dati tabella technologies

--- database/migrations/2024_02_12_135356_create_project_technology_table.php
+++ database/migrations/2024_02_12_135356_create_project_technology_table.php
@@ -12,13 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('project_technology', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('project_id');
-            $table->foreign('project_id')->references('id')->on('projects');
-            $table->unsignedBigInteger('technology_id');
-            $table->foreign('technology_id')->references('id')->on('technologies');
-            $table->string('title', 20)->unique();
-            $table->string('slug', 20);
+            $table->foreignId('project_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('technology_id')->constrained()->cascadeOnDelete();
+            $table->primary(['project_id', 'technology_id']);
             $table->timestamps();
         });
     }
