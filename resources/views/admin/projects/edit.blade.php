8546dc27fc7e94e51a602080e20eacec47099311
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-13 11:33:16 +0100

    feat: input modifica di technologies

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -17,13 +17,42 @@
         <div class="mb-3">
             <label for="type" class="form-label">Tipo</label>
             <select class="form-select" name="type_id">
-                <option selected>Seleziona un tipo</option>
+                <option value="">Seleziona un tipo</option>
                 @foreach ($types as $type)
                     <option value="{{ $type->id }}" @if (old('type_id', $project->type_id) == $type->id) selected @endif>
                         {{ $type->title }}
                     </option>
                 @endforeach
             </select>
+        </div>
+        <div class="mb-3">
+            <label for="project_img" class="form-label">Immagine</label>
+            <input type="file" class="form-control @error('project_img') is-invalid @enderror" name="project_img">
+            @error('project_img')
+                <div class="alert alert-danger mt-3">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <div>
+                <label class="form-label">Technologie</label>
+            </div>
+            @foreach ($technologies as $technology)
+                <div class="form-check form-check-inline">
+                    @if ($errors->any())
+                        <input class="form-check-input" type="checkbox" name="technologies[]"
+                            id="technology-{{ $technology->id }}" value="{{ $technology->id }}"
+                            {{ in_array($technology->id, old('technologies', [])) ? 'checked' : '' }}>
+                        <label class="form-check-label"
+                            for="technology-{{ $technology->id }}">{{ $technology->title }}</label>
+                    @else
+                        <input class="form-check-input" type="checkbox" value="{{ $technology->id }}" name="technologies[]"
+                            id="technology-{{ $technology->id }}"
+                            {{ $project->technologies->contains($technology->id) ? 'checked' : '' }}>
+                        <label class="form-check-label" for="tag-{{ $technology->id }}">{{ $technology->title }}</label>
+                    @endif
+
+                </div>
+            @endforeach
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Descrizione</label>
