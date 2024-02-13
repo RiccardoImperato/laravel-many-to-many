2d6f45a699deef59c8d07d885db80b756113b79c
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-13 17:53:37 +0100

    feat: pagina edit

---
+++ resources/views/admin/types/edit.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="d-flex justify-content-between align-items-center my-3">
+        <h2>Modifica tipo: {{ $type->title }}</h2>
+        <a href="{{ route('admin.types.index') }}" class="btn btn-primary btn-sm my-3">Indietro</a>
+    </div>
+    <form action="{{ route('admin.types.update', $type) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <div class="mb-3">
+            <label for="title" class="form-label">Titolo</label>
+            <input type="text" class="form-control @error('title') is-invalid @enderror" name="title"
+                value="{{ old('title') }}">
+            @error('title')
+                <div class="alert alert-danger mt-3">{{ $message }}</div>
+            @enderror
+        </div>
+        <button type="submit" class="btn btn-primary my-3">Submit</button>
+    </form>
+@endsection
