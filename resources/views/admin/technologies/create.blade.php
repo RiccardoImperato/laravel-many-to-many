ecf78c9b5309667de85e77c649f9e6c5ded81158
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-13 21:59:00 +0100

    feat: pagina create

---
+++ resources/views/admin/technologies/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="d-flex justify-content-between align-items-center my-3">
+        <h2>Crea nuova technologia</h2>
+        <a href="{{ route('admin.technologies.index') }}" class="btn btn-primary btn-sm my-3">Indietro</a>
+    </div>
+    <form action="{{ route('admin.technologies.store') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label for="title" class="form-label">Titolo</label>
+            <input type="text" class="form-control @error('title') is-invalid @enderror" name="title"
+                value="{{ old('title') }}">
+            @error('title')
+                <div class="alert alert-danger mt-3">{{ $message }}</div>
+            @enderror
+        </div>
+        <button type="submit" class="btn btn-primary my-3">Submit</button>
+    </form>
+@endsection
