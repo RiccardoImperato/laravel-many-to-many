8876513a8d75a7e3a99eb72f59533d00ed7e6045
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-13 22:03:45 +0100

    refactor: fix vari

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTypeRequest;
 use App\Models\Type;
-use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class TypeController extends Controller
